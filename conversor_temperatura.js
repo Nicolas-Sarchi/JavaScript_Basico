50ef0a6adc8efd559c9520f8500b0f084f1e373d
Nicolás Sarchi <nicolassarchipico@gmail.com>
2023-05-19 15:15:37 -0500

    Primer ejercicio de JS

---
+++ conversor_temperatura.js
@@ -0,0 +1,17 @@
+// Conversor de temperatura
+
+
+let celcius = (prompt("Ingrese la temperatura que desea convertir"))
+
+
+let farenheit = (parseFloat(celcius) *9/5 ) + 32
+
+
+let resultado = `${parseFloat(celcius)}°C  ➝ °F = ${farenheit}`
+
+
+document.write(resultado)
+
+
+
+
